031eb07427e1c4c4f1fd3436b2e8b9feedc3aee1
TenType <55125103+TenType@users.noreply.github.com>
2022-05-08 19:23:04 -0700

    Add trailing zeros for floats

--- src/chunk.rs
+++ src/chunk.rs
@@ -13,7 +13,7 @@ impl Display for Value {
         match self {
             Bool(value) => write!(format, "{}", value),
             Int(value) => write!(format, "{}", value),
-            Float(value) => write!(format, "{}", value),
+            Float(value) => write!(format, "{:.1}", value),
             Str(value) => write!(format, "{}", value),
         }
     }
